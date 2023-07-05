9c275edca4873cef327fdb459a998038a0c51b12
Moses <lazaromoses77@gmail.com>
2023-07-05 13:12:48 +0300

    adding hotel ranking paragraph

--- src/components/Map.js
+++ src/components/Map.js
@@ -65,7 +65,7 @@ const Map = ({ setCoordinates, hotels, setBounds, coordinates }) => {
         defaultCenter={coordinates}
         zoom={14}
         center={coordinates}
-        defaultZoom={14}
+        defaultZoom={18}
         margin={[50, 50, 50, 50]}
         options={{
             disableDefaultUI: true,
@@ -88,7 +88,8 @@ const Map = ({ setCoordinates, hotels, setBounds, coordinates }) => {
                     <FcRating fontSize={10}/>
                     <span>
                         {Number(selectedHotel.rating)}
-                    </span>
+                     </span>
+                    <p>{selectedHotel.ranking}</p>
                 </div>            
               <button onClick={handleClosePopup}>Close</button>
             </div>
@@ -105,9 +106,6 @@ const Map = ({ setCoordinates, hotels, setBounds, coordinates }) => {
          <div className="go-back-home" onClick={handleGoBackHome}>
         <FaLocationArrow />
           </div>
-       {/* <div className="reset-zoom" onClick={setDefaultZoom}>
-          <FaHome />
-        </div> */}
     </div>
   );
 };
